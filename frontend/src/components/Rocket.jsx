96d9ea7045a541c448fcea982f560fd3b15a4e3d
simransummermalik <smalik12@charlotte.edu>
2025-04-15 10:46:56 -0400

    this works, rocket can be changed, it is just a placeholder

--- frontend/src/components/Rocket.jsx
+++ frontend/src/components/Rocket.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import rocketImg from '../assets/cleanpng.png'; // i dont like this one just testing to see if it works lol
 
 const Rocket = () => {
   return (
-    <motion.div
+    <motion.img
+      src={rocketImg}
+      alt="rocket"
       initial={{ x: -300, y: 300, rotate: -45, opacity: 0 }}
       animate={{ x: '100vw', y: -200, rotate: 25, opacity: 1 }}
-      transition={{ duration: 5, ease: 'easeInOut' }}
-      className="absolute z-50"
-    >
-      <div className="text-[250px] scale-[200]">
-        🚀
-      </div>
-    </motion.div>
+      transition={{ duration: 2, ease: 'easeInOut' }}
+      style={{
+        width: '180px', //force size
+        height: '180px', //force size 
+        objectFit: 'contain',
+        position: 'absolute',
+        zIndex: 50,
+      }}
+    />
   );
 };
 
 export default Rocket;
+
+
 
 
 
