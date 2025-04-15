489312a3b6d9aefd9445f50597d2dad1ddb69766
simransummermalik <smalik12@charlotte.edu>
2025-04-15 16:38:51 -0400

    login page stuff

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css"; // download npm for tis 
 
 const Login = () => {
   return (
@@ -8,47 +9,54 @@ const Login = () => {
           <i className="fa-solid fa-user mr-2"></i> Login
         </h1>
         <hr className="mt-3" />
-        <form className="mt-3">
-          <div className="mt-3">
-            <label className="block text-base mb-2" htmlFor="email">
-              UNC Charlotte Email
+        <div className="mt-3">
+          <label htmlFor="username" className="block text-base mb-2">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
+            placeholder="Enter Username..."
+          />
+        </div>
+        <div className="mt-3">
+          <label htmlFor="password" className="block text-base mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
+            placeholder="Enter Password..."
+          />
+        </div>
+        <div className="mt-3 flex justify-between items-center">
+          <div>
+            <input type="checkbox" id="remember" />
+            <label htmlFor="remember" className="ml-1 text-sm">
+              Remember Me
             </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="name@uncc.edu"
-              className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
-              required
-            />
           </div>
-
-          <div className="mt-3 flex justify-between items-center">
-            <div>
-              <input type="checkbox" id="remember" />
-              <label htmlFor="remember" className="ml-1 text-sm">
-                Remember Me
-              </label>
-            </div>
-            <a href="#" className="text-indigo-800 font-semibold text-sm">
-              Forgot Password?
-            </a>
-          </div>
-
-          <div className="mt-5">
-            <button
-              type="submit"
-              className="border-2 border-indigo-700 bg-indigo-700 text-white py-1 w-full rounded-md hover:bg-transparent hover:text-indigo-700 font-semibold"
-            >
-              <i className="fa-solid fa-right-to-bracket mr-2"></i> Login
-            </button>
-          </div>
-        </form>
+          <a href="#" className="text-indigo-800 font-semibold text-sm">
+            Forgot Password?
+          </a>
+        </div>
+        <div className="mt-5">
+          <button
+            type="submit"
+            className="border-2 border-indigo-700 bg-indigo-700 text-white py-1 w-full rounded-md hover:bg-transparent hover:text-indigo-700 font-semibold"
+          >
+            <i className="fa-solid fa-right-to-bracket mr-2"></i> Login
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Login;
+
 
 
 
