662661af31b572d6e0e56bbf162502cd95878ff6
simransummermalik <smalik12@charlotte.edu>
2025-04-15 13:50:25 -0400

    login fixes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,75 +2,55 @@ import React from "react";
 
 const Login = () => {
   return (
-    <section className="bg-gray-50 dark:bg-gray-900 min-h-screen flex items-center justify-center">
-      <div className="w-full bg-white rounded-lg shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div className="p-6 space-y-6 sm:p-8">
-          <div className="flex justify-center">
-            <img
-              className="w-10 h-10"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-              alt="SYN logo"
+    <div className="flex justify-center items-center h-screen bg-indigo-600">
+      <div className="w-96 p-6 shadow-lg bg-white rounded-md">
+        <h1 className="text-3xl block text-center font-semibold">
+          <i className="fa-solid fa-user mr-2"></i> Login
+        </h1>
+        <hr className="mt-3" />
+        <form className="mt-3">
+          <div className="mt-3">
+            <label className="block text-base mb-2" htmlFor="email">
+              UNC Charlotte Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="name@uncc.edu"
+              className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
+              required
             />
           </div>
-          <h1 className="text-xl font-bold text-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Login to SYN
-          </h1>
-          <form className="space-y-4">
+
+          <div className="mt-3 flex justify-between items-center">
             <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                UNC Charlotte Email
+              <input type="checkbox" id="remember" />
+              <label htmlFor="remember" className="ml-1 text-sm">
+                Remember Me
               </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                required
-                placeholder="name@uncc.edu"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
-              />
             </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-                required
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
-              />
-            </div>
+            <a href="#" className="text-indigo-800 font-semibold text-sm">
+              Forgot Password?
+            </a>
+          </div>
+
+          <div className="mt-5">
             <button
               type="submit"
-              className="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
+              className="border-2 border-indigo-700 bg-indigo-700 text-white py-1 w-full rounded-md hover:bg-transparent hover:text-indigo-700 font-semibold"
             >
-              Sign in
+              <i className="fa-solid fa-right-to-bracket mr-2"></i> Login
             </button>
-            <p className="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
-              Don’t have an account?{" "}
-              <a
-                href="#"
-                className="font-medium text-purple-600 hover:underline dark:text-purple-500"
-              >
-                Sign up
-              </a>
-            </p>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
-    </section>
+    </div>
   );
 };
 
 export default Login;
+
+
 
 
 
