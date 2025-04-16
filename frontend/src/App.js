0e6c95bf61c0d217755809cfc5d783554c80d002
simransummermalik <smalik12@charlotte.edu>
2025-04-16 14:56:56 -0400

    debugging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Camera from "./pages/Camera"; // 👈 add this
 
 function App() {
   return (
@@ -8,12 +9,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/camera" element={<Camera />} /> {/* 👈 add this */}
+        {/* <Route path="/dashboard" element={<Dashboard />} /> later maybe */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
 
 
